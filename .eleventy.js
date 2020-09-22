9cf51d77aacf4d781525e3662d1a53d651db97fc
epons <ticquique@gmail.com>
2020-09-22 21:29:50 +0200

    Added initial index layout and metatags controller

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,56 @@
-module.exports = function(config) {
-    return {
-        dir: {
-            input: "src",
-            output: "dist"
-        }
+const CleanCSS = require("clean-css");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const moment = require("moment");
+const htmlmin = require("html-minifier");
+require('dotenv').config()
+
+const conf = {
+    dir: {
+        input: "src",
+        output: "dist"
     }
+};
+
+module.exports = function (config) {
+    // Directory Management
+    config.addPassthroughCopy("src/assets");
+
+
+    config.addLayoutAlias("post", "post.njk");
+    config.addLayoutAlias("base", "base.njk");
+
+    config.addFilter("cssmin", function (code) {
+        return new CleanCSS({}).minify(code).styles;
+    });
+
+    config.addPlugin(pluginRss);
+
+    // Meta Keywords: My own script
+    config.addNunjucksFilter("keyJoin",
+        function (tags, keywords) {
+            return [...(tags ? tags : []), ...(keywords ? keywords : [])].join(', ');
+        }
+    );
+
+    // Copy paste from Jérôme Coupé
+    config.addNunjucksFilter("date", function (date, format) {
+        return moment(date).format(format);
+    });
+
+    config.addTransform("htmlmin", function (content, outputPath) {
+        if (outputPath.endsWith(".html")) {
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true
+            });
+            return minified;
+        }
+
+        return content;
+    });
+
+    return conf
 }
+
+
---
+++ src/_data/metadata.js
@@ -0,0 +1,29 @@
+const assets = "/assets"
+
+module.exports = {
+    title: "Quique developer website",
+    author: "Quique",
+    email: process.env.EMAIL,
+    description: "Portfolio de Quique Pons, desarrollador de software con conocimientos de Tecnologías Web (Angular, Vue, WebRTC) e Inteligencia Artificial (Tensorflow, sklearn, Keras), así como Docker para contenerización y despliegue.",
+    photo: `profile.jpeg`,
+    assets,
+    site: `${process.env.PROTOCOL}://${process.env.DOMAIN}`,
+    now: new Date(),
+    color: {
+        "pallete_white": "#F1F2F1",
+        "pallete_yellow": "#B09652",
+        "pallete_gray": "#93A49F",
+        "pallete_brown": "#86685C",
+        "pallete_dark": "#38373B"
+    },
+    "opengraph": {
+        "languageCode": "es-ES",
+        "image": `${assets}/images/logo_name_icon.png` ,
+        "postimage": `${assets}/images/logo_name_icon.png`,
+        "latitude": "39.4675647",
+        "longitude": "-0.3792373",
+        "locality": "Valencia",
+        "countryName": "Spain"
+    },
+    "twitter": "kiblue_"
+}
